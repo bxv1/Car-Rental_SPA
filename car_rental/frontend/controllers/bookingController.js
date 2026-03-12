0e79ef0916d35ec35fad2791fea8d4d7477bb786
bxv1 <31242137@vupune.ac.in>
2026-03-12 23:26:06 +0530

    Rebuild frontend with modern responsive UX and stronger auth flow

--- car_rental/frontend/controllers/bookingController.js
+++ car_rental/frontend/controllers/bookingController.js
@@ -1,14 +1,17 @@
-app.controller("bookingController",
-function($scope,bookingService){
+app.controller("bookingController", function ($scope, bookingService) {
+  $scope.bookings = [];
+  $scope.errorMessage = "";
+  $scope.loading = true;
 
-$scope.bookings=[];
-
-bookingService.getMyBookings()
-
-.then(function(res){
-
-$scope.bookings = res.data;
-
-});
-
+  bookingService
+    .getMyBookings()
+    .then(function (res) {
+      $scope.bookings = res.data;
+    })
+    .catch(function (err) {
+      $scope.errorMessage = typeof err.data === "string" ? err.data : "Unable to load bookings.";
+    })
+    .finally(function () {
+      $scope.loading = false;
+    });
 });
--- car_rental/frontend/services/carService.js
+++ car_rental/frontend/services/carService.js
@@ -1,31 +1,15 @@
-app.service("carService",function($http){
+app.service("carService", function ($http) {
+  var API = "http://localhost:5000/api/cars";
 
-const API = "http://localhost:5000/api/cars";
+  this.getCars = function () {
+    return $http.get(API);
+  };
 
-this.getCars = function(){
+  this.addCar = function (car) {
+    return $http.post(API + "/add", car);
+  };
 
-return $http.get(API);
-
-};
-
-this.addCar = function(car){
-
-return $http.post(API+"/add",car,{
-headers:{
-Authorization:localStorage.getItem("token")
-}
-});
-
-};
-
-this.deleteCar = function(id){
-
-return $http.delete(API+"/"+id,{
-headers:{
-Authorization:localStorage.getItem("token")
-}
-});
-
-};
-
+  this.deleteCar = function (id) {
+    return $http.delete(API + "/" + id);
+  };
 });
